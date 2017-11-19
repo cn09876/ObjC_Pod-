a698db93852d3bc84e53f98556cf9051a3859c66
DevEasy <cn09876@gmail.com>
2017-11-19 10:40:31 +0800

    Initial Commit

---
+++ a1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  a1
+//
+//  Created by uosim on 2017/11/19.
+//  Copyright © 2017年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ a1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  a1
+//
+//  Created by uosim on 2017/11/19.
+//  Copyright © 2017年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
